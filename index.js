1d7769ac9e9bb6cc23480d3a48f1058e3ad6866e
MuhammadUzair <muhammaduzair066@hotmail.com>
2020-07-24 23:25:03 +0500

    fix backward compatibility issue

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-export const FetchImage = async (data) => {
-  let imageResponse = await axios.get('https://source.unsplash.com/' + data.width + 'x' + data.height + '/?' + data.type);
-  if (imageResponse && imageResponse.request.status === 200) return imageResponse.request.responseURL;
-  else return '';
+export const FetchImage = (data) => {
+  return axios.get('https://source.unsplash.com/' + data.width + 'x' + data.height + '/?' + data.type).then((res) => {
+    if (res.request.status === 200) return res.request.responseURL;
+    else return '';
+  });
 }
